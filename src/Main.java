a456f3f82984ae56f5116b38a2d4230a45d81133
Dawei <mrscarf1@outlook.com>
2018-12-26 16:32:17 +0800

    updated by gram14

--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,37 @@ import jxl.read.biff.BiffException;
 import org.sqlite.JDBC;
 
 public class Main {
+    public static void main(String[] args) {
+        String fileName = args[1];
+        String sheetName;
+        String tableName;
+        ArrayList<String> fieldNames = new ArrayList<>();
+        sheetName = args[2];
+        if(args[3] != null) {
+            tableName = args[3];
+        }
+        else if(args[2] != null) {
+            tableName = args[2];
+        }
+        else tableName = fileName;
+        File file = new File(fileName);
+        Workbook wb = null;   // excel file to read
+        try {
+            wb = Workbook.getWorkbook(file);
+        } catch (IOException | BiffException e) {
+            e.printStackTrace();
+        }
+        Sheet sheet = null;    // sheet number
+        if (wb != null) {
+            sheet = wb.getSheet(sheetName);
+            int stRows = sheet.getRows();   // number of non-empty rows
+            int stColumns = sheet.getColumns();
 
+
+            wb.close();
+        }
+        else {
+            System.out.println("WorkBook is Empty or not existed.");
+        }
+    }
 }
